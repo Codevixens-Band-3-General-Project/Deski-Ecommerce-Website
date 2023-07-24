fd7197471e79fee487a1ef28b4e8f45bb69fe605
Holy Ojakovo <ojakovoofuafo@gmail.com>
2023-07-25 00:19:35 +0100

    added a responsive navbar and a sidebar

--- src/App.js
+++ src/App.js
@@ -7,3 +7,35 @@ window.addEventListener("scroll",()=>{
         navbar.classList.add("scrol");
     }else navbar.classList.remove("scrol")
 })
+// sidebar
+let times = document.querySelector(".times");
+let show =  document.querySelector(".show");
+let hamburger =  document.querySelector(".hamburger");
+let myLinks = document.querySelectorAll(".loop-links")
+
+
+times.addEventListener("click",function(){
+    show.classList.toggle("active")
+    setTimeout(()=>{
+        hamburger.style.display = "block"
+    },100)
+})
+
+hamburger.addEventListener("click",function(){
+    show.classList.toggle("active")
+    hamburger.style.display = "none"
+   
+})
+
+
+
+myLinks.forEach((items)=>{
+    let myHeader =items.querySelector(".link-sign")
+    let myAccordion = items.querySelector(".accordion");
+    let accordionContent = items.querySelector(".accordion-content")
+    myHeader.addEventListener("click",()=>{
+        accordionContent.classList.toggle("active")
+        myAccordion.classList.toggle("active")
+    })
+})
+
